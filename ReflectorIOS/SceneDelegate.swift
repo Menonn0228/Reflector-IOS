8e8f8c59bfd705e675941da53a9244009b8af58a
Nik <menon.nikhil@icloud.com>
2020-10-22 16:16:51 -0500

    Renamed HostingController
    - Renamed to WhiteStatusBarHostingController to reflect its purpose more.

--- ReflectorIOS/SceneDelegate.swift
+++ ReflectorIOS/SceneDelegate.swift
@@ -28,7 +28,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = HostingController(rootView: ContentView())
+            window.rootViewController = WhiteStatusBarHostingController(rootView: ContentView())
             self.window = window
             window.makeKeyAndVisible()
         }
